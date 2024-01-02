2fe521c3f529cc6eccc0097eb98c8fbf2b778a89
enesdmc <enesdmcc@gmail.com>
2024-01-02 21:20:14 +0300

    project completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import img1 from "../public/img1.png";
+import img2 from "../public/img2.png";
+import img3 from "../public/img3.png";
+import img4 from "../public/img4.png";
+import imgHakkimizda from "../public/img5.jpeg";
+import imgKabiliyetlerimiz from "../public/img6.jpeg";
+import imgIletisim from "../public/img7.png";
+import clsx from "clsx";
+import { IconX } from "@tabler/icons-react";
 
 export default function Home() {
+  const images = [img1, img2, img3, img4];
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activeImage, setActiveImage] = useState(images[0]);
+  const [activeModal, setActiveModal] = useState("hakkimizda");
+
+  const headerData = {
+    Hakkımızda: "hakkimizda",
+    Kabiliyetlerimiz: "kabiliyetlerimiz",
+    İletişim: "iletisim",
+  };
+
+  const modalData = {
+    hakkimizda:
+      " Bünyesinde Makina, Havacılık ve Tasarım mühendisleri ile savunma sanayisi özel ihtiyaçlarına yönelik çözümler sağlamak amacıyla kurulan PreoTech hızlı ve müşteri odaklı çözümleri ile savunma ve otomotiv sektörüne yönelik hizmet vermektedir.",
+    kabiliyetlerimiz:
+      " Mekanik imalat , sistem tasarımı konularında uzmanlaşan Preotech CFD, FEM analizleri yanısıra Mekanik tasarım ve imalat konusunda çalışmakta olup, tedarikçi sistem yapısı ile birden fazla ihtiyaca hızlı çözüm sağlayabilmektedir.",
+    iletisim: {
+      adres:
+        "PREOTECH TEKNOLOJİ LİMİTED ŞİRKETİ ŞEHİT OSMAN AVCI MAH. MALAZGİRT 1071 CAD. 1 RELAX GÖKSU SITESI NO: 51 D İÇ KAPI NO: 203 Ankara /  Etimesgut",
+      telefon: "Telefon: 0 312 395 90 83",
+      mail: "Email: akin@preotech.com",
+    },
+  };
+
+  const handleModal = (value: string) => {
+    setIsModalOpen(true);
+    setActiveModal(value);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative h-full flex flex-col bg-[#273747] w-full ">
+      {/* header */}
+      <div className="flex items-center justify-between px-5 md:px-10 py-5 bg-[#273747] border-b border-b-white ">
+        <Image
+          src="/preotech-logo.png"
+          className="hidden md:block"
+          alt="logo"
+          width={150}
+          height={150}
+        />
+        <Image
+          src="/preotech-logo.png"
+          className="block md:hidden"
+          alt="logo"
+          width={100}
+          height={100}
+        />
+        <div className="space-x-3 md:space-x-10">
+          {Object.entries(headerData).map(([key, value], index) => (
+            <button
+              key={index}
+              onClick={() => handleModal(value)}
+              className="text-white hover:opacity-70 transition-all duration-500 font-semibold text-xs sm:text-sm md:text-md"
+            >
+              {key}
+            </button>
+          ))}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* section */}
+      <div className="w-full h-full p-10  gap-10 flex">
+        {images.map((image, index) => (
+          <Image
+            onClick={() => setActiveImage(image)}
+            key={index}
+            src={image}
+            alt="logo"
+            className={clsx(
+              " overflow-hidden cursor-pointer transition-all duration-700 object-cover rounded-3xl",
+              activeImage === image ? "flex-[3]  " : "flex-1 "
+            )}
+          />
+        ))}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {isModalOpen && (
+        <div className="absolute top-0 left-0 h-full w-full  bg-black/70 p-10 flex items-start justify-start">
+          <div className="w-full h-full  flex border border-white/70 rounded-md">
+            <Image
+              src={
+                activeModal === "hakkimizda"
+                  ? imgHakkimizda
+                  : activeModal === "kabiliyetlerimiz"
+                  ? imgKabiliyetlerimiz
+                  : imgIletisim
+              }
+              alt="logo"
+              className="object-cover hidden md:block aspect-square rounded-md flex-1"
+            />
+            <div className="relative flex-1  flex items-center justify-center p-10 text-white font-bold text-sm md:text-xl">
+              <p>
+                {" "}
+                {activeModal === "iletisim" ? (
+                  <div className="space-y-7">
+                    <p>{modalData[activeModal].adres}</p>
+                    <p>{modalData[activeModal].telefon}</p>
+                    <p>{modalData[activeModal].mail}</p>
+                  </div>
+                ) : (
+                  modalData[activeModal] 
+                )}
+              </p>
+              <button
+                className="absolute top-5 right-5 border border-white hover:opacity-70 rounded-md transition-all duration-500"
+                onClick={() => setIsModalOpen(!isModalOpen)}
+              >
+                <IconX size={30} color="white" />
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
